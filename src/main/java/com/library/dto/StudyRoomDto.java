e6027f366ef293cc1969d69fccfa7dba92705313
User <User@DESKTOP-G5BDLNE>
2021-04-07 20:21:10 +0900

    프로젝트

---
+++ src/main/java/com/library/dto/StudyRoomDto.java
@@ -0,0 +1,15 @@
+package com.library.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudyRoomDto {
+	private int SR_Id;
+	private int SR_Name;
+	private int SR_Info;
+	private int SR_Situation;
+}
---
+++ src/main/java/com/library/dto/lMemberDto.java
@@ -0,0 +1,17 @@
+package com.library.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class lMemberDto {
+	
+	private int lm_id;
+	private int lm_name;
+	private int lm_tel;
+	private int lm_email1;
+	private int lm_email2;
+}
